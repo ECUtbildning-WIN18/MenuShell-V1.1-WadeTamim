bc69e70af17064bc1fa362547439d26031dc46a8
Wade Tamim <w.tamim@hotmail.com>
2018-10-17 20:05:03 +0200

    Add project files.

---
+++ MenuShell v1.1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MenuShell_v1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
